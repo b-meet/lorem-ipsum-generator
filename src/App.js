00642a03090cfb71eb098239cc1d844e0ea0cc38
Meet <meetbhalodiya1030@gmail.com>
2022-01-02 15:35:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import data from "./data";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [number, setNumber] = useState("");
+	const [info, setInfo] = useState([]);
+
+	const errorMessage = ["Please enter value between 0 to 8"];
+
+	const handleChange = (e) => {
+		setNumber(e.target.value);
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (number >= 0 && number <= 8) {
+			setInfo(data.slice(0, number));
+		} else {
+			setInfo([errorMessage]);
+		}
+	};
+
+	return (
+		<>
+			<h2 className='heading'>Want Something Else Than Age Old Lorem Ipsum?</h2>
+			<form className='form'>
+				<label htmlFor='number'>Number Of Paragraphs: </label>
+				<input
+					type='number'
+					name='number'
+					id='number'
+					onChange={handleChange}
+					value={number}
+				/>
+				<button type='submit' className='btn' onClick={handleSubmit}>
+					Generate
+				</button>
+			</form>
+			<section className='data'>
+				{info.map((paragraph) => (
+					<p>{paragraph}</p>
+				))}
+			</section>
+		</>
+	);
 }
 
 export default App;
